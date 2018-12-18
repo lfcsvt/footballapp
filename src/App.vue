0055cfa195a2354c27a729d8843ef623b285b2b8
Lucas Fernando Correa de Souza <lucasfernandocorreadesouza@MacBook-Pro-de-lfcsvt.local>
2018-12-18 11:54:23 +0100

    fetch issues on deployment

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@ import Chat from '@/components/Chat.vue'
           'https://api.football-data.org/v2/competitions/CL/scorers/',
           'https://api.jsonbin.io/b/5c0e4d1633da576e58f7ab3c',
           'https://api.jsonbin.io/b/5c0e75f733da576e58f7c0ee',
-          'http://api.football-data.org/v2/teams/109'
+          'https://api.football-data.org/v2/teams/109'
           
         ]
         let responseArray = urls.map((url) => {
